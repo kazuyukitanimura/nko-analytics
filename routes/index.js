ec4a3848c9ed8286304f36fe43593b46591092c0
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-11-08 20:51:51 -0800

    organizing server.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  res.render('index', {
+    title: 'Node KO Analytics',
+    description: 'Node KO Analytics is a real time analytics dashboard for Node Knockout 2013.',
+    author: 'kazuyukitanimura',
+    domain: req.headers.host || 'localhost:3000'
+  });
 };
+
--- server.js
+++ server.js
@@ -7,43 +7,68 @@ require('nko')('eH7wuPnFA873a0Kl');
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var util = require('util');
+var url = require('url');
+var UglifyJS = require("uglify-js");
+
+var deepInspect = function(obj) {
+  console.log(util.inspect(obj, {
+    showHidden: true,
+    depth: 4
+    //depth: null
+  }));
+};
 
 var app = express();
 
 // all environments
 var isProduction = (process.env.NODE_ENV === 'production');
-app.set('port', process.env.PORT || isProduction? 80: 3000);
+app.set('port', process.env.PORT || isProduction ? 80: 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.compress());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express['static'](path.join(__dirname, 'public')));
+app.use(express['static'](path.join(__dirname, 'build')));
+app.use(express['static'](path.join(__dirname, 'node_modules/zeroclipboard')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+/**
+ * Routes
+ */
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-
+/**
+ * Start Server
+ */
+http.createServer(app).listen(app.get('port'), function() {
   // if run as root, downgrade to the owner of this file
   if (process.getuid() === 0) {
     fs.stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
+      if (err) {
+        return console.error(err);
+      }
       process.setuid(stats.uid);
     });
   }
 
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+/**
+ * Socket.IO
+ */
+//TODO
+
